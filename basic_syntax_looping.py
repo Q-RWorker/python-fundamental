53404acdbcabd3bcf65520edb17b5740a816cfbb
Q-RWorker <aqsha8amanah@gmail.com>
2025-07-07 06:20:28 +0700

    Looping

---
+++ basic_syntax_looping.py
@@ -0,0 +1,29 @@
+"""
+Program perulangan membaca buku
+"""
+
+# Dengan 'for'
+print("DENGAN FOR")
+jumlah_buku = 10
+print('Perintah ibu, "Baca bukumu"')
+
+buku_dibaca = 0
+print(f"Jumlah buku yang sudah dibaca {buku_dibaca} buku")
+
+for buku_dibaca in range(1,jumlah_buku+1):
+    print(f"Buku ke {buku_dibaca} sudah dibaca")
+
+print(f"Lapor ke Ibu semua buku sudah dibaca adalah {buku_dibaca} buku")
+
+# Tanpa 'for'
+print("\nTANPA FOR")
+print(f"Buku ke 1 sudah dibaca")
+print(f"Buku ke 2 sudah dibaca")
+print(f"Buku ke 3 sudah dibaca")
+print(f"Buku ke 4 sudah dibaca")
+print(f"Buku ke 5 sudah dibaca")
+print(f"Buku ke 6 sudah dibaca")
+print(f"Buku ke 7 sudah dibaca")
+print(f"Buku ke 8 sudah dibaca")
+print(f"Buku ke 9 sudah dibaca")
+print(f"Buku ke 10 sudah dibaca")
