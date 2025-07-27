2343c54ef6e95ec98deaca43ed2fe600c3c37c03
Q-RWorker <aqsha8amanah@gmail.com>
2025-07-28 05:42:33 +0700

    Sequential & Branching For / While

---
+++ basic_syntax_looping_while.py
@@ -0,0 +1,17 @@
+"""
+Program Perulangan Dengan "While"
+"""
+
+# Dengan 'While'
+
+jumlah_buku = 10
+print('Perintah ibu, "Baca bukumu"')
+
+buku_dibaca = 0
+print(f"Jumlah buku yang sudah dibaca {buku_dibaca} buku")
+
+while buku_dibaca < jumlah_buku:
+    buku_dibaca += 1
+    print(f"Baca buku ke - {buku_dibaca} yang belum dibaca")
+
+print(f"Jumlah buku yang sudah dibaca adalah {buku_dibaca}")
